b5c85e4618040b4af420ebe0337f443d7930d2a6
carbarcha <samhoward13261@gmail.com>
2024-05-01 15:17:23 +0800

    add coindesk news

--- news_craw.py
+++ news_craw.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
 import json
 
 def save_record(records:list, name:str):
@@ -69,6 +70,41 @@ def bc_crawler():
 
     return
 
+def cd_crawler():
+    records=[]
+    for page_number in range(1,101):
+        # 解析頁面新聞列表
+        base_url = f"https://www.coindesk.com/tag/bitcoin/{page_number}/"
+        pc_news = requests.get(base_url)
+        soup = BeautifulSoup(pc_news.content, "html.parser")
+        news_list = soup.find_all("div", class_="article-cardstyles__AcTitle-sc-q1x8lc-1 PUjAZ articleTextSection")
+        for page in news_list:
+            # 取得每篇文章 link
+            news_link = page.find("h6", class_="typography__StyledTypography-sc-owin6q-0 bhrWMt")
+            href = news_link.find("a").get("href")
+            # 取得文章日期
+            date_time = page.find("span", class_="typography__StyledTypography-sc-owin6q-0 iOUkmj").text
+            date_format = "%b %d, %Y at %I:%M %p"
+            date_time = datetime.strptime(date_time[:-4].replace(".", ""), date_format)
+            origin_news = requests.get(f'https://www.coindesk.com/{href}')
+            # 取得文章內容
+            soup = BeautifulSoup(origin_news.content, "html.parser")
+            contents = soup.find("div", class_="contentstyle__StyledWrapper-sc-g5cdrh-0 gkcZwU composer-content")
+            content = ""
+            if contents:
+                for text in contents:
+                    content += text.get_text()
+                record = {
+                    "time":str(date_time),
+                    "content":content
+                }
+                records.append(record)
+
+    save_record(records, "cd")
+
+    return
+
 if __name__ == "__main__":
     # pc_crawler()
-    bc_crawler()
+    # bc_crawler()
+    cd_crawler()
