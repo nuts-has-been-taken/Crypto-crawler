41a4e4a5aa881f09fbcf60d74f1f86bc9997e175
carbarcha <samhoward13261@gmail.com>
2024-05-21 14:19:49 +0800

    add crypto slate data

--- news_craw.py
+++ news_craw.py
@@ -104,7 +104,44 @@ def cd_crawler():
 
     return
 
+def cs_crawler():
+    records=[]
+    for page_number in range(1,161):
+        # 解析頁面新聞列表
+        base_url = f"https://cryptoslate.com/news/page/{page_number}/"
+        pc_news = requests.get(base_url)
+        soup = BeautifulSoup(pc_news.content, "html.parser")
+        news_list = soup.find("div", class_="list-feed slate").find_all("div", class_="list-post")
+        for page in news_list:
+            # 取得每篇文章 link
+            href = page.find("a").get("href")
+            origin_news = requests.get(href)
+            soup = BeautifulSoup(origin_news.content, "html.parser")
+            # 取得文章日期
+            date_time = soup.find("div", class_="post-date")
+            if date_time:
+                date_time = date_time.text.strip()
+                date_format = "%b %d, %Y at %I:%M %p"
+                date_time = datetime.strptime(date_time[:-4].replace(".", ""), date_format)
+                # 取得新聞內容
+                contents = soup.find("article", class_= "full-article")
+                if contents:
+                    contents = contents.find_all("p")
+                    content = ""
+                    for text in contents:
+                        content += text.get_text()
+                    record = {
+                        "time":str(date_time),
+                        "content":content
+                    }
+                    records.append(record)
+
+    save_record(records, "cs")
+
+    return
+
 if __name__ == "__main__":
     # pc_crawler()
     # bc_crawler()
-    cd_crawler()
+    # cd_crawler()
+    cs_crawler()
